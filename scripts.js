5733a042519e1c9cf5d60df94d967c454ab1fc8f
Joseph Muller <j.muller@bbk.ac.uk>
2022-12-10 10:51:47 +0000

    Draft workshop

---
+++ scripts.js
@@ -0,0 +1,110 @@
+///// 1. If you run the following code in the browser console or the VS Code console, what is the output?
+
+console.log('Leaves are falling');
+
+/* Start of your answer
+
+
+
+End of your answer */
+
+
+///// 2. How about this code? What is the output?
+
+console.log(); // 'Leaves are falling'
+console.log(/* 'Snow is freezing' */);
+console.log('Leaves are growing');
+
+/* Start of your answer
+
+
+
+End of your answer */
+
+
+///// 3. Write 3 valid JavaScript strings.
+
+// Start of your code
+
+
+
+// End of your code
+
+///// 4. Edit the following lines to make each one a valid JavaScript string, using your knowledge of quotation marks and apostrophes (" and ') and the backslash (\).
+
+// Start of your code
+
+'They made a 'Hello World' website.'
+'it doesn't matter'
+"Organize your images in "project\images""
+
+// End of your code
+
+///// 5. Write 3 valid JavaScript numbers.
+
+// Start of your code
+
+
+
+// End of your code
+
+///// 6. Write a valid Boolean value.
+
+// Start of your code
+
+
+
+// End of your code
+
+///// 7. Write code that logs the data type of this value to the console.
+
+// Start of your code
+
+'999'
+
+// End of your code
+
+
+///// 8. Combine these strings into one string using an operator.
+
+// Start of your code
+
+'Leaves are' "falling" 'quickly'
+
+// End of your code
+
+
+///// 9. Write some math that evaluates to 29.5.
+
+// Start of your code
+
+
+
+// End of your code
+
+
+///// 10. Use an operator to make the following line evaluate to `true` if the values are the same, and `false` if not.
+
+// Start of your code
+
+"Extemporaneous" 'Extemporanoeus'
+
+// End of your code
+
+
+///// 11. Use an operator to make the following line evaluate to `true` if the values are different, and `false` if they are the same.
+
+// Start of your code
+
+'Unconscionable' 'Unconcionable'
+
+// End of your code
+
+
+///// 12. Use an operator to make the following line evaluate to `true`.
+
+// Start of your code
+
+1000 999
+
+// End of your code
